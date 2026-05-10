fdf77846aae8745e4c84c47695b33aac526d2a87
AnpanMan17 <laurasurcoenzo@gmail.com>
2026-05-10 20:50:07 -0300

    fix(uefi): implement uefi_call_wrapper to avoid ABI crash and keep screen visible

--- TP3-a/aplicacion.c
+++ TP3-a/aplicacion.c
@@ -1,16 +1,19 @@
 #include <efi.h>
-#include <efilib.h>
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    InitializeLib(ImageHandle, SystemTable);
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Iniciando analisis de seguridad...\r\n");
-    
+    uefi_call_wrapper(SystemTable->ConOut->OutputString, 2,
+                      SystemTable->ConOut,
+                      L"Iniciando analisis de seguridad...\r\n");
+
     // Inyección de un software breakpoint (INT3)
     unsigned char code[] = { 0xCC };
-    
+
     if (code[0] == 0xCC) {
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Breakpoint estatico alcanzado.\r\n");
+        uefi_call_wrapper(SystemTable->ConOut->OutputString, 2,
+                          SystemTable->ConOut,
+                          L"Breakpoint estatico alcanzado.\r\n");
     }
-    
+
+    uefi_call_wrapper(SystemTable->BootServices->Stall, 1, 3000000);
     return EFI_SUCCESS;
 }
