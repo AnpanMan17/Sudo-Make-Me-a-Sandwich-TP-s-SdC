8daf3b6e47f893fa84505f22a143c0b33f527ef2
Saqib Daniel <saqib13062003@gmail.com>
2026-05-19 11:42:12 -0300

    Adding the package.deb

---
+++ desafio1/hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World! %s\n", "I will be a pakage .deb");
+    return 0;
+}
--- hello_world.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello World!\n" , "I will be a pakage .deb");
-    return 0;
-}
